4d4fb1ed322585e475d243bdcd94450cfeee0c06
mohamed0yaser <mohamedyaser2023@gmail.com>
2025-06-24 12:34:14 +0300

    solve conflicts

--- lib/features/business_account/product/view/widgets/Service_submit_buttons.dart
+++ lib/features/business_account/product/view/widgets/Service_submit_buttons.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+
 class ServiceSubmitButtons extends StatelessWidget {
   final GlobalKey<FormState> formKey;
 
